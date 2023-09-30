4760605ad9f33b68c564b957227bd7c73ca19ffc
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-09-30 15:39:15 +0530

    Add files via upload

---
+++ 1-A-1(method-2).c
@@ -0,0 +1,12 @@
+//Calculate addition of two numbers using scanf
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter the value of a:" );
+	scanf("%d",&a);
+	printf("Enter the value of b:" );
+	scanf("%d",&b);
+	sum=a+b;
+	printf("Addition is=%d",sum);
+}
