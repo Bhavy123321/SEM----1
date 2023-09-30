bb39bb0570582ece4d0ea82cfcaa25d5c3d12eec
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-09-30 15:44:24 +0530

    Add files via upload

---
+++ 2-A-3.c
@@ -0,0 +1,16 @@
+//Check whether given number is positive or negative.
+#include<stdio.h>
+void main()
+{
+	int no;
+	printf("enter any integer except 0");
+	scanf("%d",&no);
+	if(no>0)
+	{
+		printf("entered number is possitive");
+	}
+	else
+	{
+		printf("entered number is negative");
+	}
+}
