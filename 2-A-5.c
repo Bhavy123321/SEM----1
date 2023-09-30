8842aee9e41603bbb5972f8c0f4aec8b3997743c
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-09-30 15:45:47 +0530

    Add files via upload

---
+++ 2-A-5.c
@@ -0,0 +1,16 @@
+//Find out largest number from given two numbers.
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter the value of a and b: " );
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	{
+		printf("a is large");
+	}
+	else
+	{
+		printf("b is large");
+	}
+}
