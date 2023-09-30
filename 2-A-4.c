5ac0360332523cda2b66a60ee47c1fafc9a8e399
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-09-30 15:45:25 +0530

    Add files via upload

---
+++ 2-A-4.c
@@ -0,0 +1,17 @@
+//Check whether the given number is odd or even.
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter an integer");
+	scanf("%d",&n);
+	if(n%2!=0)
+	{
+		printf("no is odd");
+	}
+	else
+	{
+		printf("no is even");
+	}
+	
+}
