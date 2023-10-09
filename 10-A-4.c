2d833742986defac9b71dad71262c16bf1deb070
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-10-09 18:05:13 +0530

    Add files via upload

---
+++ 10-A-4.c
@@ -0,0 +1,14 @@
+//Print digits of given number.
+#include<stdio.h>
+void main()
+{
+	int n,temp=0;
+	printf("Enter any integer : ");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+	 temp=n%10;
+	 printf("%d\n",temp);
+	 n=n/10;
+	}
+}
