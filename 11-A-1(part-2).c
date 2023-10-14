c9c8f86b32addd1b8bb323eba941e41302b61b3d
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-10-14 12:55:54 +0530

    Add files via upload

---
+++ 11-A-1(part-2).c
@@ -0,0 +1,12 @@
+//Print 1 to 10 then modify program print 1 to n.
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	printf("Enter the value of x: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",i);
+	}
+}
