e87615624b920ab60548f61570d4bb3e6cfd68b2
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-10-19 20:10:41 +0530

    Add files via upload

---
+++ 13-A-1(d).c
@@ -0,0 +1,24 @@
+/*
+    1
+   1 2
+  1 2 3
+ 1 2 3 4
+1 2 3 4 5
+*/
+#include<stdio.h>
+void main()
+{
+	int i,j,k,n=5;
+	for(i=0;i<n;i++)
+	{
+		for(k=5;k>i;k--)
+			{
+				printf(" ");
+			}
+		for(j=0;j<=i;j++)
+		{
+			printf("%d ",(j+1));	
+		}
+		printf("\n");	
+	}
+}
