cae0e36337b2e677d9fd4e9efe58a1d949deceb8
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2023-10-29 14:30:42 +0530

    Add files via upload

---
+++ 14-B-1.c
@@ -0,0 +1,23 @@
+//Count numbers higher than the average of an array. 
+#include <stdio.h>
+
+void main()
+{
+    int n,i,sum=0,count=0;
+    printf("enter no. of values: ");
+    scanf("%d",&n);
+    int a[n];
+   for(i=0;i<n;i++)
+    {
+       printf("enter value of a[%d]:",i);
+       scanf ("%d",&a[i]);
+       sum=sum+a[i];
+    }
+   float avg=sum/n;
+   for(i=0;i<n;i++)
+    {
+       if(a[i]>avg)
+       count++;
+    }
+    printf("%d",count);
+}
